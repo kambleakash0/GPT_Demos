8bee2cd3a18acce43f2d5232e02905664b6824e7
Akash Kamble <kambleakash0@gmail.com>
2023-03-12 00:04:44 +0530

    updated UI to have conversation view

--- app.py
+++ app.py
@@ -9,23 +9,28 @@ messages = [
 ]
 
 
-def chatbot(input):
+def predict(input):
+    global messages
     if input:
         messages.append({"role": "user", "content": input})
         chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
         reply = chat.choices[0].message.content
         messages.append({"role": "assistant", "content": reply})
+        reply = [(messages[i]["content"], messages[i+1]["content"]) for i in range(1, len(messages)-1, 2)]
         return reply
 
+with gr.Blocks(css="#chatbot .overflow-y-auto{height:700px}") as demo:
+    demo.title="FinanceLLM Chatbot"
+    gr.Markdown("## FinanceLLM Chatbot demo")
 
-inputs = gr.components.Textbox(lines=7, label="Chat with AI")
-outputs = gr.components.Textbox(label="Reply")
+    chatbot = gr.Chatbot(elem_id="chatbot", label="FinGPT").style(container=False)
+    # state = gr.State([])
 
-gr.Interface(
-    fn=chatbot,
-    inputs=inputs,
-    outputs=outputs,
-    title="FinanceLLM Chatbot",
-    description="Ask anything you want",
-    allow_flagging="never",
-).launch(share=False)
+    with gr.Row():
+        txt = gr.Textbox(show_label=False, placeholder="Type a message and press enter").style(container=False)
+
+    txt.submit(predict, txt, chatbot)
+    # txt.submit(lambda: "", None, txt) # Pythonic way of clearing text after submit
+    txt.submit(None, None, txt, _js="() => {''}") # Javascript way of clearing text after submit, faster than Pythonic way
+
+demo.launch()
